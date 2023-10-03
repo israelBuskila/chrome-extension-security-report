3e9c6053e2ab75ff01949fac857277b4a16e40e9
israelbu <Israel.Buskila@amdocs.com>
2023-10-03 19:29:01 +0300

    delet unnecessary code

--- maliciousDownloadServer/routes/report.js
+++ maliciousDownloadServer/routes/report.js
@@ -18,30 +18,3 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const MaliciousDownload = require("../models/maliciousDownload");
-
-// router.post("/", (req, res) => {
-//   const { filename, url, timestamp } = req.body;
-
-//   const newReport = new MaliciousDownload({
-//     filename,
-//     url,
-//     timestamp,
-//   });
-
-//   newReport
-//     .save()
-//     .then((report) => {
-//       console.log("Malicious download reported:", report);
-//       res.status(200).json({ message: "Malicious download reported successfully." });
-//     })
-//     .catch((error) => {
-//       console.error("Error reporting malicious download:", error);
-//       res.status(500).json({ message: "Error reporting malicious download." });
-//     });
-// });
-
-// module.exports = router;
